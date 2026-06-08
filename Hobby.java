e080f3a02c72d94f38c01d97d96f283cc11b30c2
aws-kentalbores <kent.albores@awsys-i.com>
2026-06-08 15:11:00 +0800

    function: display trainee hobby

--- Hobby.java
+++ Hobby.java
@@ -5,3 +5,8 @@ public class Hobby {
 		displayTraineeHobbyC();
 	}
 }
+
+public static void displayTraineeHobbyX() {
+	System.out.println("Trainee X’s Hobbies:");
+	// Hobbies of Trainee X will be added here
+}
